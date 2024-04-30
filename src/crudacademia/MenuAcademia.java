d0a08c06347bca0958b048be2357a2f57404f0a8
Artur Miranda <artcamir12@gmail.com>
2024-04-29 21:57:50 -0300

    feat: modificações no menu

--- src/crudacademia/MenuAcademia.java
+++ src/crudacademia/MenuAcademia.java
@@ -13,8 +13,8 @@ public class MenuAcademia {
     private static Pessoa professorInstrutor = null;
 
     public static void main(String[] args) {
-        criarAdministrador();
-        criarProfessorInstrutor();
+        criarContaAdministrador();
+        criarContaProfessor();
 
         while (true) {
             if (academia == null) {
@@ -29,37 +29,71 @@ public class MenuAcademia {
         }
     }
 
-    private static void criarAcademia() {
-        System.out.println("----- Criação da Academia -----");
-        academia = criarAcademia(scanner);
-        if (academia == null) {
-            System.out.println("Não foi possível criar a academia. Encerrando o programa.");
-            System.exit(1);
-        }
+    private static void criarContaAdministrador() {
+        System.out.println("----- Criação da Conta do Administrador -----");
+        administrador = criarAdministrador();
     }
 
-    private static void criarAdministrador() {
-        System.out.println("----- Criação do Administrador -----");
-        administrador = criarUsuario("Administrador");
+    private static void criarContaProfessor() {
+        System.out.println("----- Criação da Conta do Professor/Instrutor -----");
+        professorInstrutor = criarProfessorInstrutor();
     }
 
-    private static void criarProfessorInstrutor() {
-        System.out.println("----- Criação do Professor/Instrutor -----");
-        professorInstrutor = criarUsuario("Professor/Instrutor");
-
-    }
-
-    private static Pessoa criarUsuario(String tipoUsuario) {
-        System.out.println("Informe os dados para criar o " + tipoUsuario + ":");
+    private static Pessoa criarAdministrador() {
+        System.out.println("Informe os dados para criar o Administrador:");
         System.out.print("Nome: ");
-        String nome = scanner.nextLine();
+        String nome = scanner.nextLine().trim();
+        while (nome.isEmpty()) {
+            System.out.println("Nome não pode estar vazio. Informe novamente:");
+            nome = scanner.nextLine().trim();
+        }
+
         System.out.print("Login: ");
-        String login = scanner.nextLine();
+        String login = scanner.nextLine().trim();
+        while (login.isEmpty()) {
+            System.out.println("Login não pode estar vazio. Informe novamente:");
+            login = scanner.nextLine().trim();
+        }
+
         System.out.print("Senha: ");
-        String senha = scanner.nextLine();
+        String senha = scanner.nextLine().trim();
+        while (senha.isEmpty()) {
+            System.out.println("Senha não pode estar vazia. Informe novamente:");
+            senha = scanner.nextLine().trim();
+        }
+
         Date dataCriacao = Calendar.getInstance().getTime();
         Date dataModificacao = Calendar.getInstance().getTime();
-        return new Pessoa(0, nome, 'M', null, login, senha, tipoUsuario, dataCriacao, dataModificacao);
+        return new Pessoa(numAlunos++, nome, 'M', null, login, senha, "Administrador", dataCriacao, dataModificacao);
+    }
+
+    private static Pessoa criarProfessorInstrutor() {
+        System.out.println("Informe os dados para criar o Professor/Instrutor:");
+        System.out.print("Nome: ");
+        String nome = scanner.nextLine().trim();
+        while (nome.isEmpty()) {
+            System.out.println("Nome não pode estar vazio. Informe novamente:");
+            nome = scanner.nextLine().trim();
+        }
+
+        System.out.print("Login: ");
+        String login = scanner.nextLine().trim();
+        while (login.isEmpty()) {
+            System.out.println("Login não pode estar vazio. Informe novamente:");
+            login = scanner.nextLine().trim();
+        }
+
+        System.out.print("Senha: ");
+        String senha = scanner.nextLine().trim();
+        while (senha.isEmpty()) {
+            System.out.println("Senha não pode estar vazia. Informe novamente:");
+            senha = scanner.nextLine().trim();
+        }
+
+        Date dataCriacao = Calendar.getInstance().getTime();
+        Date dataModificacao = Calendar.getInstance().getTime();
+        return new Pessoa(numAlunos++, nome, 'M', null, login, senha, "Professor/Instrutor", dataCriacao,
+                dataModificacao);
     }
 
     private static boolean efetuarLogin() {
@@ -96,8 +130,12 @@ public class MenuAcademia {
             System.out.println("----- Menu da Academia -----");
             System.out.println("1. Exibir Detalhes da Academia");
             System.out.println("2. Criar Aluno");
-            System.out.println("3. Exibir Dados do Aluno por ID");
-            System.out.println("4. Sair");
+            System.out.println("3. Exibir Todos os Alunos");
+            System.out.println("4. Exibir Dados do Aluno por ID");
+            System.out.println("5. Criar outro Administrador");
+            System.out.println("6. Criar outro Professor/Instrutor");
+            System.out.println("7. Remover um aluno por ID");
+            System.out.println("8. Sair");
             System.out.print("Escolha uma opção: ");
 
             int opcao = scanner.nextInt();
@@ -111,9 +149,21 @@ public class MenuAcademia {
                     criarAluno();
                     break;
                 case 3:
-                    exibirDadosAlunoPorId();
+                    exibirTodosAlunos();
                     break;
                 case 4:
+                    exibirDadosAlunoPorId();
+                    break;
+                case 5:
+                    criarContaAdministrador();
+                    break;
+                case 6:
+                    criarContaProfessor();
+                    break;
+                case 7:
+                    removerAluno();
+                    break;
+                case 8:
                     System.out.println("Deslogando...");
                     loggedOut = true;
                     break;
@@ -123,7 +173,8 @@ public class MenuAcademia {
         }
     }
 
-    private static Academia criarAcademia(Scanner scanner) {
+    private static Academia criarAcademia() {
+        System.out.println("----- Criação da Academia -----");
         System.out.println("Informe os detalhes da academia:");
         System.out.print("ID: ");
         int idAcademia = scanner.nextInt();
@@ -134,7 +185,8 @@ public class MenuAcademia {
         String enderecoAcademia = scanner.nextLine();
         Date dataCriacao = Calendar.getInstance().getTime(); // Obter a data atual
         Date dataModificacao = Calendar.getInstance().getTime(); // Obter a data atual
-        return new Academia(idAcademia, nomeAcademia, enderecoAcademia, dataCriacao, dataModificacao);
+        academia = new Academia(idAcademia, nomeAcademia, enderecoAcademia, dataCriacao, dataModificacao);
+        return academia;
     }
 
     private static void exibirDetalhesAcademia(Academia academia) {
@@ -148,13 +200,15 @@ public class MenuAcademia {
 
     private static void criarAluno() {
         System.out.println("Informe os detalhes do aluno:");
-        System.out.print("ID: ");
-        int idAluno = scanner.nextInt();
-        scanner.nextLine(); // Limpar o buffer do teclado
         System.out.print("Nome: ");
-        String nomeAluno = scanner.nextLine();
+        String nomeAluno = scanner.nextLine().trim();
+        while (nomeAluno.isEmpty()) {
+            System.out.println("Nome não pode estar vazio. Informe novamente:");
+            nomeAluno = scanner.nextLine().trim();
+        }
         System.out.print("Sexo (M/F): ");
         char sexoAluno = scanner.nextLine().charAt(0);
+
         System.out.print("Data de nascimento (dd/MM/yyyy): ");
         String dtNascimentoStr = scanner.nextLine();
 
@@ -166,21 +220,43 @@ public class MenuAcademia {
             System.out.println("Formato de data inválido! Use o formato dd/MM/yyyy.");
             return;
         }
+
         Date dataCriacao = Calendar.getInstance().getTime(); // Obter a data atual
         Date dataModificacao = Calendar.getInstance().getTime(); // Obter a data atual
         String tipoUsuario = "Aluno";
 
         // Solicitar ao professor/administrador para criar login e senha para o aluno
         System.out.print("Criar login para o aluno: ");
-        String loginAluno = scanner.nextLine();
+        String loginAluno = scanner.nextLine().trim();
+        while (loginAluno.isEmpty()) {
+            System.out.println("Login não pode estar vazio. Informe novamente:");
+            loginAluno = scanner.nextLine().trim();
+        }
+
         System.out.print("Criar senha para o aluno: ");
-        String senhaAluno = scanner.nextLine();
+        String senhaAluno = scanner.nextLine().trim();
+        while (senhaAluno.isEmpty()) {
+            System.out.println("Senha não pode estar vazia. Informe novamente:");
+            senhaAluno = scanner.nextLine().trim();
+        }
 
         // Criar um novo aluno com login e senha
-        Pessoa aluno = new Pessoa(idAluno, nomeAluno, sexoAluno, dtNascimento, loginAluno, senhaAluno, tipoUsuario, dataCriacao, dataModificacao);
+        Pessoa aluno = new Pessoa(numAlunos, nomeAluno, sexoAluno, dtNascimento, loginAluno, senhaAluno, tipoUsuario,
+                dataCriacao, dataModificacao);
         alunos[numAlunos] = aluno;
         System.out.println("Aluno criado com sucesso!");
         numAlunos++;
+    }
+
+    private static void exibirTodosAlunos() {
+        if (numAlunos == 0) {
+            System.out.println("Nenhum aluno cadastrado ainda.");
+        } else {
+            System.out.println("Lista de Alunos:");
+            for (int i = 0; i < numAlunos; i++) {
+                System.out.println("ID: " + alunos[i].getId() + ", Nome: " + alunos[i].getNome());
+            }
+        }
     }
 
     private static void exibirDadosAlunoPorId() {
@@ -201,5 +277,33 @@ public class MenuAcademia {
         if (!encontrado) {
             System.out.println("Aluno não encontrado.");
         }
+    }
+
+    private static void removerAlunoPorId(int id) {
+        boolean encontrado = false;
+        for (int i = 0; i < numAlunos; i++) {
+            if (alunos[i].getId() == id) {
+                // Remover o aluno encontrado movendo os alunos subsequentes para trás no array
+                for (int j = i; j < numAlunos - 1; j++) {
+                    alunos[j] = alunos[j + 1];
+                }
+                // Definir o último elemento como nulo e decrementar o contador de alunos
+                alunos[numAlunos - 1] = null;
+                numAlunos--;
+                System.out.println("Aluno removido com sucesso.");
+                encontrado = true;
+                break;
+            }
+        }
+        if (!encontrado) {
+            System.out.println("Aluno com ID " + id + " não encontrado.");
+        }
+    }
+
+    private static void removerAluno() {
+        System.out.print("Informe o ID do aluno que deseja remover: ");
+        int idAluno = scanner.nextInt();
+        scanner.nextLine(); // Limpar o buffer do teclado
+        removerAlunoPorId(idAluno);
     }
 }
