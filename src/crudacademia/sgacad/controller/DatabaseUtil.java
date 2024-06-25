e92a21687b96b94199c016b1b38a4985ee59c9ed
Artur Miranda <artcamir12@gmail.com>
2024-06-25 14:52:31 -0300

    fechando conexao com banco no final

--- src/crudacademia/sgacad/controller/DatabaseUtil.java
+++ src/crudacademia/sgacad/controller/DatabaseUtil.java
@@ -19,7 +19,7 @@ public class DatabaseUtil {
 
     public static void createDatabase() {
         try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
-                Statement statement = connection.createStatement()) {
+             Statement statement = connection.createStatement()) {
             statement.executeUpdate("DROP DATABASE IF EXISTS " + DATABASE_NAME);
             statement.executeUpdate("CREATE DATABASE " + DATABASE_NAME);
         } catch (SQLException e) {
@@ -28,8 +28,11 @@ public class DatabaseUtil {
     }
 
     public static void createTables() {
-        try (Connection connection = getConnection();
-                Statement statement = connection.createStatement()) {
+        Connection connection = null;
+        Statement statement = null;
+        try {
+            connection = getConnection();
+            statement = connection.createStatement();
             statement.executeUpdate("USE " + DATABASE_NAME);
             statement.executeUpdate("CREATE TABLE IF NOT EXISTS academia (" +
                     "id INT AUTO_INCREMENT PRIMARY KEY," +
@@ -66,7 +69,6 @@ public class DatabaseUtil {
                     "login VARCHAR(50) NOT NULL UNIQUE," +
                     "senha VARCHAR(50) NOT NULL," +
                     "data_modificacao DATE)");
-
             statement.executeUpdate("CREATE TABLE IF NOT EXISTS ProfessoresInstrutores (" +
                     "id INT PRIMARY KEY AUTO_INCREMENT," +
                     "nome VARCHAR(100) NOT NULL," +
@@ -75,7 +77,6 @@ public class DatabaseUtil {
                     "login VARCHAR(50) NOT NULL UNIQUE," +
                     "senha VARCHAR(50) NOT NULL," +
                     "data_modificacao DATE)");
-
             statement.executeUpdate("CREATE TABLE IF NOT EXISTS aluno_pagamento_mensalidade (" +
                     "id INT AUTO_INCREMENT PRIMARY KEY," +
                     "mensalidade_vigente DOUBLE NOT NULL," +
@@ -87,7 +88,6 @@ public class DatabaseUtil {
                     "data_criacao DATE NOT NULL," +
                     "data_modificacao DATE NOT NULL," +
                     "FOREIGN KEY (id_aluno) REFERENCES Alunos(id))");
-
             statement.executeUpdate("CREATE TABLE IF NOT EXISTS entradas_aluno (" +
                     "id INT AUTO_INCREMENT PRIMARY KEY," +
                     "id_aluno INT NOT NULL," +
@@ -95,9 +95,15 @@ public class DatabaseUtil {
                     "data_criacao DATE NOT NULL," +
                     "data_modificacao DATE NOT NULL," +
                     "FOREIGN KEY (id_aluno) REFERENCES Alunos(id))");
-
         } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+            try {
+                if (statement != null) statement.close();
+                if (connection != null) connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
     }
 
